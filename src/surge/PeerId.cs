3f2afa0a145139a3952004ad45ee30c51997db44
Simon Nattress <nattress@gmail.com>
2017-05-12 01:08:21 -0700

    PeerId Generator

---
+++ src/surge/PeerId.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surge
+{
+    public class PeerId
+    {
+        private static readonly string RandomCharacterSource = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.-";
+        private static readonly string ClientId = "Sg-";
+
+        // Todo: Read from config
+        private static readonly string VersionNumber = "0.0.1";
+
+        private static string s_peerId;
+
+        public static string Id
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(s_peerId))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    
+                    sb.Append(ClientId + VersionNumber);
+
+                    int numberOfRandomCharactersNeeded = 20 - sb.Length;
+                    var random = new Random();
+
+                    for (int i = 0; i < numberOfRandomCharactersNeeded; i++)
+                    {
+                        sb.Append(random.Next(RandomCharacterSource.Length));
+                    }
+
+                    s_peerId = sb.ToString();
+                }
+
+                return s_peerId;
+            }
+        }
+    }
+}
